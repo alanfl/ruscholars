f8020103a147023e222859d4ef445d1a3f4babc3
alanfl <alanfl81199@gmail.com>
2018-10-05 14:45:25 -0400

    Change file read method and fix author data parse.

--- getAuthors.js
+++ getAuthors.js
@@ -6,15 +6,12 @@ var authorsTemp = []
 
 // Reads in a file of provided author ids and filters relevant data to an output file for use
 // in the RUScholars system
+// For now, the entire file will be read into memory, but as the file size expands, a file stream
+// will need to be used
+async function readAuthorFile(fileName) {
+	authorsTemp = fs.readFileSync('test.txt').toString().split("\n");
 
-function readAuthorFile(fileName) {
-	fs.readFile(fileName, function(err, data) {
-	    if(err) throw err;
-	    authorsTemp = parseInt(data.toString().split("\n"));
-
-	    // DEBUG
-	    console.log("On call: ", authorsTemp)
-	});
+	console.log(authorsTemp);
 }
 
 async function getAuthorData(authorId) {
@@ -22,10 +19,17 @@ async function getAuthorData(authorId) {
 	// Assemble URL
 	var url = "https://api.elsevier.com/content/author/author_id/"+authorId+"?apiKey=53dbebb3541a6f89921f6495a85d039e";
 
+	console.log("url = " + url)
+
 	// Handle response
 	let authorPromise = axios.get(url)
 		.then(response => {
-			return response.data["author-retrieval-response"].coredata;
+
+			// DEBUG
+			// console.log(response.data["author-retrieval-response"])
+
+			return response.data["author-retrieval-response"][0];
+
 		})
 		.catch(error => {
 			console.log(error);
@@ -34,17 +38,33 @@ async function getAuthorData(authorId) {
 	return authorPromise;
 }
 
-function createAuthor(authorData) {
+async function createAuthor(authorData) {
+
+
+	// DEBUG
+
+	// console.log("***AUTHOR DATA***")
+	// console.log(authorData)
+
+	// console.log("=================")
+	// console.log("***COREDATA***")
+	// console.log(authorData["coredata"]);
+
+	// END DEBUG
+
 	var author = 
 	{
-		"authorId": parseInt(authorData["dc:identifier"].substring(10)),
-		"document-count": authorData["document-count"],
-		"cited-by-count": authorData["cited-by-count"],
-		"citation-count": authorData["citation-count"],
-		"affiliation-current-id": authorData["affiliation-current-id"].id,
+		"authorId": parseInt( (authorData["coredata"]['dc:identifier']).substring(10)),
+		"eid": authorData["coredata"]["eid"],
+		"document-count": authorData["coredata"]["document-count"],
+		"cited-by-count": authorData["coredata"]["cited-by-count"],
+		"citation-count": authorData["coredata"]["citation-count"],
+		"affiliation-current-id": authorData["affiliation-current"]["@id"],
 		"affiliation-current-name": authorData["author-profile"]["affiliation-current"]["affiliation"]["ip-doc"]["afdispname"],
 		"subject-areas": authorData["subject-areas"]
 	}
+
+	console.log(author)
 }
 
 async function populateAuthors() {
@@ -52,7 +72,11 @@ async function populateAuthors() {
 
 	for(i in authorsTemp) {
 		var data = (await getAuthorData(authorsTemp[i]));
-		authors.set(authorsTemp[i], createAuthor(data));
+
+		// DEBUG
+		// console.log(data);
+
+		 authors.set(authorsTemp[i], await createAuthor(data));
 	}
 
 	// DEBUG
